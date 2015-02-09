488316f9bbebc6875ee02249c9d634b8a657bbe7
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-02-09 18:12:16 +0000

    Added Ingredient Id To Food Items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206161919) do
+ActiveRecord::Schema.define(version: 20150209181039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150206161919) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ingredient_id"
   end
 
   create_table "ingredients", force: true do |t|
