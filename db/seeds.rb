9ee77276ebe22a946f81c870215dd21395c572fc
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-02-10 14:39:17 +0000

    added ingredients and food seed data not tested yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Deleting all existing data"
+
+User.delete_all
+FoodItem.delete_all
+Ingredient.delete_all
+
+
 ellie = User.create!(first_name: "Ellie", last_name: "McGregor", email: "ellie.ga@gmail.com", password: "password", role: :admin)
 
 50.times do
   user = User.create!(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", email: "#{Faker::Internet.email}", password: "password", user_image: "#{Faker::Avatar.image}", role: :normal)
 end
+
+ingredients = Ingredient.all
+ingredient = ingredients.sample
+users = User.all
+user = users.sample
+
+50.times do
+  food_item = FoodItem.create!(name: "#{Faker::Name.title}", expiry_date: "#{Faker::Date.forward(5)}", food_item_image: "#{Faker::Avatar.image}", user: users.sample, ingredient: ingredients.sample )
+end
+
+50.times do
+  ingredient = Ingredient.create!(name: "#{Faker::Name.name}", unit: "#{Faker::Name.name}",default_expiry_in_days: "#{Faker::Date.forward(7)}")
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
