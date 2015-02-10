d3af404faaa9bd49a39802b1f96cbb741a9767a3
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-02-10 15:13:44 +0000

    modified seed data and added ingredients id to views to that i can see it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ FoodItem.delete_all
 Ingredient.delete_all
 
 
-ellie = User.create!(first_name: "Ellie", last_name: "McGregor", email: "ellie.ga@gmail.com", password: "password", role: :admin)
+ellie = User.create!(first_name: "Ellie", last_name: "McGregor", email: "ellie@ga.com", password: "password", role: :admin)
 
 50.times do
-  user = User.create!(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", email: "#{Faker::Internet.email}", password: "password", user_image: "#{Faker::Avatar.image}", role: :normal)
+  user = User.create!(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", email: "#{Faker::Internet.email}", password: "password", user_image: "#{Faker::Food.image}", role: :normal)
 end
 
 ingredients = Ingredient.all
@@ -24,12 +24,14 @@ users = User.all
 user = users.sample
 
 50.times do
-  food_item = FoodItem.create!(name: "#{Faker::Name.title}", expiry_date: "#{Faker::Date.forward(5)}", food_item_image: "#{Faker::Avatar.image}", user: users.sample, ingredient: ingredients.sample )
+  ingredient = Ingredient.create!(name: "#{Faker::Name.name}", unit: "#{Faker::Name.name}",default_expiry_in_days: "#{Faker::Date.forward(7)}")
 end
 
 50.times do
-  ingredient = Ingredient.create!(name: "#{Faker::Name.name}", unit: "#{Faker::Name.name}",default_expiry_in_days: "#{Faker::Date.forward(7)}")
+  food_item = FoodItem.create!(name: "#{Faker::Name.title}", expiry_date: "#{Faker::Date.forward(5)}", food_item_image: "#{Faker::Food.image}", user: users.sample, ingredient: ingredients.sample )
 end
+
+
 
 
 
