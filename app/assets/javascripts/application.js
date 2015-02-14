9f8692faca81e29de44674fd414e155393d6cdae
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-02-14 11:35:28 +0000

    added animate sign up button - customisation needed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -105,5 +105,18 @@ jQuery(function(){
   jQuery.mark.jump();
 });
 
+///// animate sign up button
+$(function() {
+  var animationClasses = 'animated alternate iteration zoomOut';
+  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+  $('.animate-trigger').on('click',function() {
+    $('.animate-target').addClass(animationClasses).one(animationEnd,function() {
+      $(this).removeClass(animationClasses);
+    });
+  });
+});
+
+
 
 
