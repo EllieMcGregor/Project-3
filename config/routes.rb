129642ba22193c10dac3f6cb730bfa2363df2991
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-02-14 14:21:12 +0000

    added kaminari to routes and added views for it; ;

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
 
   resources :food_items
 
+  resources :recipes do
+    get 'page/:page', action: :index, on: :collection
+  end
+
+
   devise_for :users
   root to: "home#index"
 end
